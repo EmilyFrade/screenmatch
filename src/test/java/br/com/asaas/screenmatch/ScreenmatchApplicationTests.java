0734455aeefde9dd2f38d6541c0e6d25938d8750
Emily Frade <emilyfrade12@gmail.com>
2024-05-06 17:24:57 -0300

    Commit inicial

---
+++ src/test/java/br/com/asaas/screenmatch/ScreenmatchApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.asaas.screenmatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScreenmatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
