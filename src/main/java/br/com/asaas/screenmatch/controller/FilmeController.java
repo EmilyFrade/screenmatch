49e6ac4703fd1cc4d42e5c05fe7252feb5d6f30c
Emily Frade <emilyfrade12@gmail.com>
2024-05-06 18:51:35 -0300

    Listando os filmes com thymeleaf

--- src/main/java/br/com/asaas/screenmatch/controller/FilmeController.java
+++ src/main/java/br/com/asaas/screenmatch/controller/FilmeController.java
@@ -3,6 +3,7 @@ package br.com.asaas.screenmatch.controller;
 import br.com.asaas.screenmatch.domain.filme.DadosCadastroFilme;
 import br.com.asaas.screenmatch.domain.filme.Filme;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,9 +17,15 @@ public class FilmeController {
 
     private List<Filme> filmes = new ArrayList<>();
 
-    @GetMapping
+    @GetMapping("/formulario")
     public String carregaPaginaFormulario(){
         return "filmes/formulario";
+    }
+
+    @GetMapping
+    public String carregaPaginaListagem(Model model){
+        model.addAttribute("lista", filmes);
+        return "filmes/listagem";
     }
 
     @PostMapping
